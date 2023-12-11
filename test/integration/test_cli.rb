cb3f7f26796fd96a20cd0e4178d194220a5c483e
Bayan Khalili <bayan.net@gmail.com>
2023-12-11 16:42:12 +1100

    Full paths are not necessary.

--- test/integration/test_cli.rb
+++ test/integration/test_cli.rb
@@ -5,7 +5,7 @@ require "open3"
 
 class TestCLI < Minitest::Test
   def test_example_1
-    input = File.read(File.expand_path("test/examples/example1.txt"))
+    input = File.read("test/examples/example1.txt")
     stdout, stderr, status = Open3.capture3("bundle exec exe/robot_sim", stdin_data: input)
     assert_equal 0, status.exitstatus
     assert_equal "Output: 0,1,NORTH\n", stdout
@@ -13,7 +13,7 @@ class TestCLI < Minitest::Test
   end
 
   def test_example_2
-    input = File.read(File.expand_path("test/examples/example2.txt"))
+    input = File.read("test/examples/example2.txt")
     stdout, stderr, status = Open3.capture3("bundle exec exe/robot_sim", stdin_data: input)
     assert_equal 0, status.exitstatus
     assert_equal "Output: 0,0,WEST\n", stdout
@@ -21,7 +21,7 @@ class TestCLI < Minitest::Test
   end
 
   def test_example_3
-    input = File.read(File.expand_path("test/examples/example3.txt"))
+    input = File.read("test/examples/example3.txt")
     stdout, stderr, status = Open3.capture3("bundle exec exe/robot_sim", stdin_data: input)
     assert_equal 0, status.exitstatus
     assert_equal "Output: 3,3,NORTH\n", stdout
@@ -29,7 +29,7 @@ class TestCLI < Minitest::Test
   end
 
   def test_big_example
-    stdout, stderr, status = Open3.capture3("gunzip -c #{File.expand_path("test/examples/big_example.txt.gz")} | bundle exec exe/robot_sim --size 1000000")
+    stdout, stderr, status = Open3.capture3("gunzip -c test/examples/big_example.txt.gz | bundle exec exe/robot_sim --size 1000000")
     assert_equal 0, status.exitstatus
     assert_equal "Output: 3,3,NORTH\n", stdout
     assert_equal "", stderr
